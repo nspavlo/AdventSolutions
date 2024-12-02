e9f36f758e9c5526f9a85510f62f70ced36b8d65
Jans Pavlovs <jans.pavlovs@gmail.com>
2024-12-02 11:21:40 +0200

    Add day 2 part 2 solution

--- AdventSolutions/2024/Day02.swift
+++ AdventSolutions/2024/Day02.swift
@@ -11,10 +11,10 @@ struct Day02: Day {
     let dayNumber = 2
     let entries = Input.entriesFromTextFile(named: "Day02")
 
-    func part1() -> Int {
-        let increaseMin = 1
-        let increaseMax = 3
+    let increaseMin = 1
+    let increaseMax = 3
 
+    func part1() -> Int {
         func isSequenceValid(_ numbers: [Int]) -> Bool {
             var isSequenceIncreasing: Bool?
             for (previous, current) in zip(numbers, numbers.dropFirst()) {
@@ -32,6 +32,7 @@ struct Day02: Day {
 
                 isSequenceIncreasing = isIncreasing
             }
+
             return true
         }
 
@@ -44,6 +45,64 @@ struct Day02: Day {
     }
 
     func part2() -> Int {
-        return 2
+        func isSequenceIncreasing(_ numbers: [Int]) -> Bool {
+            var increasingNumbers = 0
+            var decreasingNumbers = 0
+            for (previous, current) in zip(numbers, numbers.dropFirst()) {
+                let hop = current - previous
+                if hop > 0 {
+                    increasingNumbers += 1
+                } else if hop < 0 {
+                    decreasingNumbers += 1
+                }
+            }
+
+            return increasingNumbers > decreasingNumbers
+        }
+
+        func isSequenceValid(_ numbers: [Int], isSequenceIncreasing: Bool) -> Bool {
+            for (previous, current) in zip(numbers, numbers.dropFirst()) {
+                let currentHop = current - previous
+                let currentHopAbs = abs(currentHop)
+
+                guard currentHopAbs >= increaseMin && currentHopAbs <= increaseMax else {
+                    return false
+                }
+
+                let isIncreasing = currentHop > 0
+                if isSequenceIncreasing != isIncreasing {
+                    return false
+                }
+            }
+
+            return true
+        }
+
+        func validateWithTolerance(_ numbers: [Int]) -> Bool {
+            let isSequenceIncreasing = isSequenceIncreasing(numbers)
+
+            if isSequenceValid(numbers, isSequenceIncreasing: isSequenceIncreasing) {
+                return true
+            }
+
+            var isValid = false
+
+            for index in 0 ..< numbers.count {
+                var adjustedNumbers = numbers
+                adjustedNumbers.remove(at: index)
+                let isAdjustedValid = isSequenceValid(adjustedNumbers, isSequenceIncreasing: isSequenceIncreasing)
+                isValid = isAdjustedValid
+                if isValid { break }
+            }
+
+            return isValid
+        }
+
+        let safeReportsCount = entries.reduce(0) { count, entry in
+            let numbers = entry.split(separator: " ").compactMap { Int($0) }
+            return validateWithTolerance(numbers) ? count + 1 : count
+        }
+
+        return safeReportsCount
     }
 }
