83a1210e68467001f02b1c91dd5aa5ad34fde17f
Jans Pavlovs <jans.pavlovs@gmail.com>
2024-12-03 11:49:42 +0200

    Add day 3 part 2 solution

--- AdventSolutions/2024/Day03.swift
+++ AdventSolutions/2024/Day03.swift
@@ -13,11 +13,11 @@ struct Day03: Day {
 
     func part1() -> Int {
         let pattern = #"mul\((\d+),(\d+)\)"#
-        
+
         guard let regex = try? NSRegularExpression(pattern: pattern) else {
             return 0
         }
-        
+
         var sum = 0
         for entry in entries {
             let matches = regex.matches(in: entry, range: NSRange(entry.startIndex..., in: entry))
@@ -34,6 +34,34 @@ struct Day03: Day {
     }
 
     func part2() -> Int {
-        return 2
+        let pattern = #"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"#
+
+        guard let regex = try? NSRegularExpression(pattern: pattern) else {
+            return 0
+        }
+
+        var sum = 0
+        var isOperationEnabled = true
+        for entry in entries {
+            let matches = regex.matches(in: entry, range: NSRange(entry.startIndex..., in: entry))
+            for match in matches {
+                let range = Range(match.range, in: entry)
+                if let range, entry[range] == "do()" {
+                    isOperationEnabled = true
+                } else if let range, entry[range] == "don't()" {
+                    isOperationEnabled = false
+                }
+
+                if isOperationEnabled {
+                    if let range1 = Range(match.range(at: 1), in: entry), let range2 = Range(match.range(at: 2), in: entry) {
+                        if let num1 = Int(entry[range1]), let num2 = Int(entry[range2]) {
+                            sum += num1 * num2
+                        }
+                    }
+                }
+            }
+        }
+
+        return sum
     }
 }
